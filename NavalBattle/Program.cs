19f50bfdd1d1e94500f384f51ce27f4725b27dc0
Daniil Shlonov <shlonov@nixsolutions.com>
2019-12-18 16:27:06 +0200

    Add project files.

---
+++ NavalBattle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NavalBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
